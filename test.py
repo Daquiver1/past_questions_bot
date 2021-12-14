43181724a1f2bee93de6d1539f42cc2860273f07
Daquiver <Cabrokwa11@gmail.com>
2021-12-14 16:24:01 +0000

    updated gitignore

---
+++ test.py
@@ -0,0 +1,35 @@
+# TODO: MAKE YOUR CODE OOP FORMAT
+# TODO: CHANGE THE REFERENCES TO XPATH
+# TODO: COMMENT YOUR CODE
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+with open("credentials.txt", "r") as cred:
+	user_name = cred.readline()
+	password = cred.readline()
+
+print(f"""You have 2 options
+	- Enter the course code of the subjecct (DCIT 212). Ensure there
+	 is a space between the name and the number.
+	- Enter the name of the subject (Vectors and Mechanics)""")
+name = input()
+
+s =Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=s)
+url = "https://balme.ug.edu.gh/past.exampapers/index.php?p=member"
+driver.get(url)
+login = driver.find_element_by_name("memberID")
+pwd = driver.find_element_by_name("memberPassWord")
+pwd.send_keys(password)
+login.send_keys(user_name)
+search = driver.find_element_by_name("keywords")
+search_b = driver.find_element_by_name("search")
+search.send_keys(name)
+search_b.click()
+
+
+time.sleep(50)
+#driver.close()f
