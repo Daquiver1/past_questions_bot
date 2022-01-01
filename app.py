4688cfd29212c618eed65eb69eacd47c2e265f6a
Christian Abrokwa <Cabrokwa11@gmail.com>
2022-01-01 11:15:03 +0000

    updated app

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import telegram
 from thebot.bot import get_response
 TOKEN = "5092060662:AAGbACVVEUlo67Up4Xyh7v3dMjf61MOMisI"
 URL = "https://past-questions-bot.herokuapp.com/"
-gloabl bot 
+global bot 
 bot = telegram.Bot(token = TOKEN)
 
 # Start the flask app
@@ -12,25 +12,25 @@ app = Flask(__name__)
 
 @app.route('/{}'.format(TOKEN), methods=['POST'])
 def respond():
-    # retrieve the message in JSON and then transform it to Telegram object
-    update = telegram.Update.de_json(request.get_json(force=True), bot)
+	# retrieve the message in JSON and then transform it to Telegram object
+	update = telegram.Update.de_json(request.get_json(force=True), bot)
 
 	# get the chat_id to be able to respond to the same user
-    chat_id = update.message.chat.id
+	chat_id = update.message.chat.id
 
-    # get the message id to be able to reply to this specific message
-    msg_id = update.message.message_id
+	# get the message id to be able to reply to this specific message
+	msg_id = update.message.message_id
 
 	# Telegram understands UTF-8, so encode text for unicode compatibility
-    text = update.message.text.encode('utf-8').decode()
-    print("got text message :", text)
+	text = update.message.text.encode('utf-8').decode()
+	print("got text message :", text)
 
 	# here we call our super AI
-    response = get_response(text)
-    #response = handle_message(text)
+	response = get_response(text)
+	#response = handle_message(text)
 	# now just send the message back
-    # notice how we specify the chat and the msg we reply to
-    bot.sendMessage(chat_id=chat_id, text=response, reply_to_message_id=msg_id)
+	# notice how we specify the chat and the msg we reply to
+	bot.sendMessage(chat_id=chat_id, text=response, reply_to_message_id=msg_id)
 
 	return 'ok'
 
