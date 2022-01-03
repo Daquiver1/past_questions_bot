7f357fd659a13dcdf66534edcb64396ed7487d3b
Christian Abrokwa <Cabrokwa11@gmail.com>
2022-01-03 22:04:45 +0000

    Cleaned code

--- bot.py
+++ bot.py
@@ -8,6 +8,10 @@ PORT = int(os.environ.get("PORT", "8443"))
 TOKEN = os.environ.get("TOKEN")
 
 def start(update, context):
+	"""
+	Start command
+	"""
+
 	update.message.reply_text(f"""
 		Hello {update.message.from_user.username}
 		Welcome to Daquiver's Past Question bot
@@ -18,6 +22,10 @@ def start(update, context):
 		""")
 
 def help(update, context):
+	"""
+	Help command
+	"""
+
 	update.message.reply_text("""
 	The following commands are available:
 
@@ -29,11 +37,45 @@ def help(update, context):
 	""")
 
 def donate(update, context):
+	"""
+	Donate command
+	"""
+
 	update.message.reply_text(f"""
 		Number: 0547642843
 		""")
 
+def contact(update, context):
+	"""
+	Contact command
+	"""
+
+	update.message.reply_text(f"""
+		You can contact me through the following
+		Gmail: Cabrokwa11@gmail.com
+		Telegram: @Daquiver
+		Github: https://github.com/Daquiver1
+		""")
+
+def about(update,context):
+	"""
+	About command
+	"""
+
+	update.message.reply_text(f"""
+		Okay so this is a simple bot nothing fancy or anything, it basically scrapes the ug past questions site(https://balme.ug.edu.gh/past.exampapers/index.php)
+		and returns the past question.
+		It can only download past questions on the ug site. If the past question isn't available then legon haven't uploaded it to their site.
+		I built it because students find it difficult to access the site(you need to register) and also most don't know about the site(weird, I know), 
+		so this is a simplified version. Also I knew building this will challenge me and make me a better developer(it did.) So yeah. 
+		""")
+
+
 def get_chat_id(update, context):
+	"""
+	A function that returns the chat id of the user
+	"""
+
     chat_id = -1
 
     if update.message is not None:
@@ -50,13 +92,13 @@ def get_chat_id(update, context):
 
 def clean_name(pasco_name):
 	"""
-	A function to ask user for name of past_question.
-	It takes in the course name and course code.
-	It returns the the course name and course code concatenated.
+	A function that returns the cleaned name of user's text.
+	Returns None if user doesn't satisfy a criteria.
 
 	Output: String type.
 	"""
-	if len(pasco_name.split()) == 1:		# numbers and text combined(no space)
+
+	if len(pasco_name.split()) == 1:		# if it's numbers and text combined(no space)
 		temp = re.compile("([a-zA-Z]+)([0-9]+)")
 		res = temp.match(pasco_name)
 		if res == None:
@@ -65,8 +107,8 @@ def clean_name(pasco_name):
 	else:
 		res = pasco_name.split()
 
-	pasco_name = res[0]
-	pasco_code = res[-1]
+	pasco_name = res[0]			# Course name
+	pasco_code = res[-1]		# Course code
 
 	if pasco_name.isalpha() == False:
 		res = None
@@ -88,27 +130,12 @@ def clean_name(pasco_name):
 	return pasco
 
 
-def contact(update, context):
-	update.message.reply_text(f"""
-		You can contact me through the following
-		Gmail: Cabrokwa11@gmail.com
-		Telegram: @Daquiver
-		Github: https://github.com/Daquiver1
-		""")
-
-def about(update,context):
-	update.message.reply_text(f"""
-		Okay so this is a simple bot nothing fancy or anything, it basically scrapes the ug past questions site(https://balme.ug.edu.gh/past.exampapers/index.php)
-		and returns the past question.
-		It can only download past questions on the ug site. If the past question isn't available then legon haven't uploaded it to their site.
-		I built it because students find it difficult to access the site(you need to register) and also most don't know about the site(weird, I know), 
-		so this is a simplified version. Also I knew building this will challenge me and make me a better developer(it did.) So yeah. 
-		""")
-
-
 def button(update, context):
+	"""
+	Callback function, takes the user's choice and returns a past question assigned to their choice.
+	"""
 	choice = update.callback_query
-	choice.answer()
+	choice.answer()			# callback queries have to be answered. (don't fully understand why)
 	site = link_of_pasco()
 	choice.edit_message_text(text=f"Selected option: {choice.data}")
 	choice.edit_message_text("Downloading past question, gimme a sec")
@@ -116,53 +143,61 @@ def button(update, context):
 		file = download_pasco(site, choice.data)
 		choice.edit_message_text("Uploading past question, gimme a sec")
 		context.bot.sendDocument(chat_id=get_chat_id(update, context), document=open(file, 'rb'))
-	except OSError:
+	except:
 		choice.edit_message_text("Yikes, we encountered an error. Try again. If error persists view the help command and contact me.")
 
 
 def handle_message(update, context):
+	"""
+	A function to handle user messages. 
+	Takes the text and returns options of the avaliable of the text. 
+	Assuming it matched the criteria specified in clean_name()
+	"""
+
 	options = []
 	update.message.reply_text(f"You said {update.message.text}")
-	name12 = clean_name(update.message.text)
-	if name12 == None:
+	new_text = clean_name(update.message.text)
+	if new_text == None:
 		update.message.reply_text("Please enter a valid past question name (eg. ugbs 104, dict 202)")
 		return None
 
-	update.message.reply_text(f"Checking for {name12} past questions")
+	update.message.reply_text(f"Checking for {new_text} past questions")
 	time.sleep(1)
-	search_for_pasco(name12)
+	search_for_pasco(new_text)
 	lists = display_pascos()
-	if len(lists) == 0:
-		print(lists)
-		update.message.reply_text(f"Unfortunately, there are no past questions available for {name12}")
+	if len(lists) == 0:				# Check if there are past questions available for users text.
+		update.message.reply_text(f"Unfortunately, there are no past questions available for {new_text}")
 		return None
 
-	update.message.reply_text("Yaay!!, we got some.")
-	print(lists)
+	update.message.reply_text("Yaay! we got some.")
 
 	for i in range(len(lists)):
-		update.message.reply_text(str(i+1) + " " + lists[i])
+		update.message.reply_text(str(i+1) + " " + lists[i])	# display available past questions.
 		options.append(InlineKeyboardButton(text=str(i+1), callback_data=str(i+1)))
 
 	reply_markup = InlineKeyboardMarkup([options])
-	context.bot.send_message(chat_id=get_chat_id(update, context), text='What would you like to download?', reply_markup=reply_markup)
+	context.bot.send_message(chat_id=get_chat_id(update, context), text='What one do you want to download?', reply_markup=reply_markup)
 
+def main(): 
+	updater = telegram.ext.Updater(TOKEN, use_context=True)
+	disp = updater.dispatcher
+	disp.add_handler(telegram.ext.CommandHandler("start", start))
+	disp.add_handler(telegram.ext.CommandHandler("help", help))
+	disp.add_handler(telegram.ext.CommandHandler("about", about))
+	disp.add_handler(telegram.ext.CommandHandler("donate", donate))
+	disp.add_handler(CallbackQueryHandler(button))
+	disp.add_handler(telegram.ext.CommandHandler("contact", contact)) 
+	disp.add_handler(telegram.ext.MessageHandler(telegram.ext.Filters.text, handle_message))
 
-updater = telegram.ext.Updater(TOKEN, use_context=True)
-disp = updater.dispatcher
-disp.add_handler(telegram.ext.CommandHandler("start", start))
-disp.add_handler(telegram.ext.CommandHandler("help", help))
-disp.add_handler(telegram.ext.CommandHandler("about", about))
-disp.add_handler(telegram.ext.CommandHandler("donate", donate))
-disp.add_handler(CallbackQueryHandler(button))
-disp.add_handler(telegram.ext.CommandHandler("contact", contact)) 
-disp.add_handler(telegram.ext.MessageHandler(telegram.ext.Filters.text, handle_message))
+	# for polling
+	#updater.start_polling()
+	#"""
+	updater.start_webhook(listen="0.0.0.0",
+							port = PORT,
+							url_path=TOKEN,
+							webhook_url= "https://past-questions-bot.herokuapp.com/" + TOKEN)
+	#"""
+	updater.idle()
 
-#updater.start_polling()
-#"""
-updater.start_webhook(listen="0.0.0.0",
-						port = PORT,
-						url_path=TOKEN,
-						webhook_url= "https://past-questions-bot.herokuapp.com/" + TOKEN)
-#"""
-updater.idle()
+if __name__ == '__main__':
+	main()
--- test.py
+++ test.py
@@ -1,7 +1,5 @@
 # TODO: INCLUDE THE FILES IN THE NEXT PAGES (later though)
 # TODO: ADD AN ALL PAST QUESTIONS FEATURE(later though)
-# from selenium.webdriver.chrome.service import Service
-# from webdriver_manager.chrome import ChromeDriverManager
 
 
 from selenium import webdriver
@@ -13,35 +11,31 @@ from bs4 import BeautifulSoup
 import requests
 import time, re
 
+# Used when polling. 
+
+# from selenium.webdriver.chrome.service import Service
+# from webdriver_manager.chrome import ChromeDriverManager
+#PATH = "path to folder"
+#s = Service(ChromeDriverManager().install())
+#options = webdriver.ChromeOptions()
+#options.headless = True	
+#driver = webdriver.Chrome(service=s, options = options)
+
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 PATH = "/tmp"
-
-
-
-#PATH = "C:\\Users\\Anita Agyepong\\Documents\\Daquiver's Quivers\\Python\\past_questions_bot\\past_questions"
 PROFILE = {"plugins.plugins_list": [{"enabled": False, "name": "Chrome PDF Viewer"}],
  	               "download.default_directory": PATH, "download.extensions_to_open": ""}	# Open externally not with chrome's pdf viewer
-#s = Service(ChromeDriverManager().install())
-#options = webdriver.ChromeOptions()
-#options.headless = True	
 chrome_options.add_experimental_option('prefs', PROFILE)
-#driver = webdriver.Chrome(service=s, options = options)
-# with open("cred\\credentials.txt", "r") as cred: # Retrieve credentials
-# 	user_name = cred.readline()
-# 	password = cred.readline()
-
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-
 URL = "https://balme.ug.edu.gh/past.exampapers/index.php?p=member"
 USER_NAME = os.environ.get("USER_NAME")
 PASSWORD = os.environ.get("PASSWORD")
-print(USER_NAME)
-print(PASSWORD)
 
+# Log in
 driver.get(URL)
 login = driver.find_element(By.NAME, "memberID")
 pwd = driver.find_element(By.NAME, "memberPassWord")
@@ -49,14 +43,16 @@ button = driver.find_element(By.NAME, "logMeIn")
 pwd.send_keys(PASSWORD)
 login.send_keys(USER_NAME) 			# The newline is interpreted as enter
 button.click()
-print("We have began")
+print("Logged in succesfully")
 
 def newest(path):
+	"""
+	A function to that returns the path of the downloaded file.
+	It takes in the downlaod directory path and returns the most recent one.
+	"""
 
     files = os.listdir(path)
-    print(files)
     paths = [os.path.join(path, basename) for basename in files if basename.endswith(".pdf")] 
-    print(paths)
 
     return max(paths, key=os.path.getctime)
     
@@ -78,10 +74,10 @@ def display_pascos():
 	A function to retrieve the names, year and semester of 
 	past questions displayed.
 
-	inputs: A website address.
-	output: A print out of the name, year and semester of
+	output: A print-out of the name, year and semester of
 			past question.
 	"""
+
 	page = requests.get(driver.current_url)
 
 	soup = BeautifulSoup(page.content, 'lxml')
@@ -94,16 +90,15 @@ def display_pascos():
 	    sem1 = i.find("div", class_ = "customField collationField")
 	    temp.append(pasco1.get_text() + "\n" + year1.get_text() + "\n" + sem1.get_text())
 
-	print("These are the past questions available")
 	return temp
 
 def link_of_pasco():
 	"""
 	A function to retrieve the links of all the past questions displayed.
 
-	inputs: The website address.
 	output: A dictionary containing the index and the past question link.
 	"""
+
 	page = requests.get(driver.current_url)
 
 	soup = BeautifulSoup(page.content, "lxml")
@@ -118,39 +113,30 @@ def link_of_pasco():
 def download_pasco(links, choice):
 	"""
 	A function to download past questions.
-	It takes in the link of the past questions available.
-	User selects a past question.
-	Downloads the past question.
+	It takes in the links of the past questions and the users choice.
+	Returns the path of the downloaded past question. 
 
-	Input: Dictionary type.
-	Output: A pdf?
 	"""
 
 	for i, v in links.items():
 		if int(choice) == i:
-			driver.get(v)					 # Move to the url of the selected option
+			driver.get(v)					 # Move to the url of users choice.
 
 	file = driver.find_element(By.CLASS_NAME, "openPopUp")
-	driver.execute_script("arguments[0].click();", file) 
+	driver.execute_script("arguments[0].click();", file)		# screen displayed is a frame, so adapts to a frame.
 	wait = WebDriverWait(driver, 10)
 	wait.until(EC.frame_to_be_available_and_switch_to_it((By.CLASS_NAME, "cboxIframe")))
 	wait.until(EC.element_to_be_clickable((By.ID, "download"))).click()
 	driver.back()
-	print("Downloading file, wait a min")
-	time.sleep(2)
+	time.sleep(2)							# wait for file to be downloaded before moving on.
 	file = newest(PATH)
 	return file
-
-
 
 if __name__ == '__main__':
 	name = input("Please enter the course name : ")
 	search_for_pasco(name)
-	yaw = display_pascos()
-	nii = link_of_pasco()
-	boat = input("Number please: ")
-	download_pasco(nii, boat)
-	#links = link_of_pasco()
-	#download_pasco(links)
-	#driver.quit()
+	questions = display_pascos()
+	pasco_links = link_of_pasco()
+	user_choice = input("Number please: ")
+	download_pasco(pasco_links, user_choice)
 
