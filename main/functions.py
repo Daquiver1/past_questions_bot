b07830a1087ef30326b2222fd249914eea382a34
Christian Abrokwa <Cabrokwa11@gmail.com>
2022-08-01 17:16:19 +0000

    minor changes

--- main/functions.py
+++ main/functions.py
@@ -12,9 +12,9 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-from webdriver_manager.chrome import ChromeDriverManager
 
 # Used when polling.
+# from webdriver_manager.chrome import ChromeDriverManager
 # PATH = "path to folder"
 # s = Service(ChromeDriverManager().install())
 # options = webdriver.ChromeOptions()
