daf6ae78acbf06a54ed6227e792d9b57a1b7bba0
Christian Abrokwa <Cabrokwa11@gmail.com>
2023-04-30 19:40:34 +0000

    Testing...

--- src/functions.py
+++ src/functions.py
@@ -288,7 +288,9 @@ class Functions:
             wait.until(
                 EC.frame_to_be_available_and_switch_to_it((By.CLASS_NAME, "cboxIframe"))
             )
-            wait.until(EC.element_to_be_clickable((By.ID, "download"))).click()
+            self.driver.find_element(By.ID, "download").click()
+
+            # wait.until(EC.element_to_be_clickable((By.ID, "download"))).click()
 
             logger.info("Downloading file...")
             file_logger.info(f"{past_question_link} has been downloaded.")
