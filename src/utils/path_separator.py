b8247c5bca3facc248ee261bc3e918765a903a30
Christian Abrokwa <Cabrokwa11@gmail.com>
2023-04-30 13:18:30 +0000

    Testing path separtor

--- src/utils/path_separator.py
+++ src/utils/path_separator.py
@@ -3,6 +3,7 @@ import platform
 
 def get_file_separator():
     """Return the appropriate file path separator based on the operating system."""
+    return '/'
     if platform.system() == 'Windows':
         return '\\'
     else:
