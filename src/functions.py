da87ff70afac99329ff7f6b1cddb7e7bd09231f1
Christian Abrokwa <Cabrokwa11@gmail.com>
2023-04-30 18:14:18 +0000

    Fixed error where all function wasn't working

--- src/functions.py
+++ src/functions.py
@@ -67,7 +67,7 @@ class Functions:
         }
         options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
         options.add_experimental_option("prefs", self.PROFILE)
-        options.add_argument("--headless=new")
+        # options.add_argument("--headless=new")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
 
