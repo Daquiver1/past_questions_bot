107dfac981236fb89fbaa0dd1cfac5ba492d864e
Christian Abrokwa <Cabrokwa11@gmail.com>
2023-05-27 19:42:34 +0000

    Built migration

--- src/db/migrations/versions/ccf36cec622e_create_main_tables.py
+++ src/db/migrations/versions/ccf36cec622e_create_main_tables.py
@@ -5,14 +5,13 @@ Revises:
 Create Date: 2023-05-26 19:05:17.551904
 
 """
-from alembic import op
 from typing import Tuple
 
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
-revision = 'ccf36cec622e'
+revision = "ccf36cec622e"
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -23,16 +22,12 @@ def timestamps(indexed: bool = False) -> Tuple[sa.Column, sa.Column]:
     return (
         sa.Column(
             "created_at",
-            sa.TIMESTAMP(timezone=True),
-            server_default=sa.func.now(),
-            nullable=False,
+            sa.DateTime(timezone=True),
             index=indexed,
         ),
         sa.Column(
             "updated_at",
-            sa.TIMESTAMP(timezone=True),
-            server_default=sa.func.now(),
-            nullable=False,
+            sa.DateTime(timezone=True),
             index=indexed,
         ),
     )
@@ -42,63 +37,124 @@ def create_users_table() -> None:
     """Create User table."""
     op.create_table(
         "users",
-        sa.Column("id", sa.String, primary_key=True),
-        sa.Column("username", sa.Text, unique=True, nullable=False, index=True),
+        sa.Column(
+            "id",
+            sa.Integer,
+            primary_key=True,
+        ),
+        sa.Column("uuid", sa.Text, unique=True, nullable=False, index=True),
+        sa.Column("username", sa.Text, unique=True, index=True),
         sa.Column("telegram_id", sa.Text, unique=True, nullable=False, index=True),
         sa.Column("is_subscribed", sa.Integer, nullable=False, default=0),
-        sa.Column(
-            "is_eligible", sa.Integer, nullable=False, default=0
-        ),
+        sa.Column("is_eligible", sa.Integer, nullable=False, default=0),
+        sa.Column("balance", sa.Float, nullable=False, default=0.0),
         *timestamps(),
     )
-    op.execute("DROP TRIGGER IF EXISTS update_timestamp")
+    op.execute("DROP TRIGGER IF EXISTS user_trigger_created_at")
+    op.execute("DROP TRIGGER IF EXISTS user_trigger_updated_at")
     op.execute(
         """
-        CREATE TRIGGER update_timestamp
-        AFTER UPDATE ON users
-        FOR EACH ROW
-        BEGIN
-            UPDATE users
-            SET updated_at = datetime('now')
-            WHERE id = OLD.id;
-        END;
+            CREATE TRIGGER user_trigger_created_at
+            AFTER INSERT ON users
+            BEGIN
+                UPDATE users SET created_at = DATETIME('now') where id = new.id;
+                UPDATE users SET updated_at = DATETIME('now') where id = new.id;
+            END;
+
+"""
+    )
+    op.execute(
+        """
+          CREATE TRIGGER user_trigger_updated_at
+            AFTER UPDATE ON users
+            BEGIN
+                UPDATE users SET updated_at = DATETIME('now') where id = new.id;
+            END;
+        """
+    )
+
+
+def create_past_question_categories_table() -> None:
+    """Create Past Question Categories table."""
+    op.create_table(
+        "past_question_categories",
+        sa.Column("id", sa.Integer, primary_key=True),
+        sa.Column("uuid", sa.Text, unique=True, nullable=False, index=True),
+        sa.Column("category_name", sa.String, nullable=False, unique=True),
+        *timestamps(),
+    )
+    op.execute("DROP TRIGGER IF EXISTS past_question_categories_trigger_created_at")
+    op.execute("DROP TRIGGER IF EXISTS past_question_categories_trigger_updated_at")
+    op.execute(
+        """
+            CREATE TRIGGER past_question_categories_trigger_created_at
+            AFTER INSERT ON past_question_categories
+            BEGIN
+                UPDATE past_question_categories SET created_at = DATETIME('now') WHERE id = new.id;
+                UPDATE past_question_categories SET updated_at = DATETIME('now') WHERE id = new.id;
+            END;
+"""
+    )
+    op.execute(
+        """
+          CREATE TRIGGER past_question_categories_trigger_updated_at
+            AFTER UPDATE ON past_question_categories
+            BEGIN
+                update past_question_categories SET updated_at = DATETIME('now') WHERE id = new.id;
+            END;
         """
     )
 
 
 def create_past_questions_table() -> None:
-    """Create User table."""
+    """Create Past Question table."""
     op.create_table(
         "past_questions",
-        sa.Column("id", sa.String, primary_key=True),
+        sa.Column("id", sa.Integer, primary_key=True),
+        sa.Column("uuid", sa.Text, unique=True, nullable=False, index=True),
         sa.Column("course_code", sa.String, nullable=False, index=True),
-        sa.Column("course_description", sa.String, nullable=False, index=True),
+        sa.Column("course_name", sa.String, nullable=False, index=True),
         sa.Column("past_question_url", sa.String, nullable=False),
         sa.Column("lecturer", sa.String, nullable=False, index=True),
         sa.Column("year", sa.String, nullable=False, index=True),
         sa.Column("semester", sa.String, nullable=False, index=True),
+        sa.Column(
+            "category_id",
+            sa.Integer,
+            sa.ForeignKey("past_question_categories.id", ondelete="CASCADE"),
+        ),
         *timestamps(),
     )
-    op.execute("DROP TRIGGER IF EXISTS update_timestamp")
+    op.execute("DROP TRIGGER IF EXISTS past_question_trigger_created_at")
+    op.execute("DROP TRIGGER IF EXISTS past_question_trigger_updated_at")
     op.execute(
         """
-        CREATE TRIGGER update_timestamp
-        AFTER UPDATE ON past_questions
-        FOR EACH ROW
-        BEGIN
-            UPDATE past_questions
-            SET updated_at = datetime('now')
-            WHERE id = OLD.id;
-        END;
+            CREATE TRIGGER past_question_trigger_created_at
+            AFTER INSERT ON past_questions
+            BEGIN
+                UPDATE past_questions SET created_at = DATETIME('now') WHERE id = new.id;
+                UPDATE past_questions SET updated_at = DATETIME('now') WHERE id = new.id;
+            END;
+"""
+    )
+    op.execute(
+        """
+          CREATE TRIGGER past_question_trigger_updated_at
+            AFTER UPDATE ON past_questions
+            BEGIN
+                update past_questions SET updated_at = DATETIME('now') WHERE id = new.id;
+            END;
         """
     )
 
 
 def upgrade() -> None:
     create_users_table()
+    create_past_question_categories_table()
     create_past_questions_table()
 
 
 def downgrade() -> None:
     op.drop_table("users")
+    op.drop_table("past_question_categories")
     op.drop_table("past_questions")
