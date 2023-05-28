361fbf795260ca813f06a8fdfce0fa8919e5e8d4
Christian Abrokwa <Cabrokwa11@gmail.com>
2023-05-28 11:33:32 +0000

    WRote tests for s3 bucket

---
+++ tests/s3/conftest.py
@@ -0,0 +1,32 @@
+import pytest
+
+from models.past_question import PastQuestionCreate
+from services.s3.s3 import delete_folder
+
+
+@pytest.fixture(scope="module")
+def setup_s3(mock_category: str):
+    yield
+    delete_folder(folder_name=mock_category)  # type: ignore
+
+
+@pytest.fixture
+def mock_tempfile() -> str:
+    return "tests/s3/mock_file.txt"
+
+
+@pytest.fixture(scope="module")
+def mock_category() -> str:
+    return "mock_category"
+
+
+@pytest.fixture
+def mock_file() -> PastQuestionCreate:
+    return PastQuestionCreate(
+        course_code="mock_code",
+        course_name="mock_name",
+        lecturer="mock_lecturer",
+        past_question_url="mock_url",
+        semester="mock_semester",
+        year="mock_year",
+    )
